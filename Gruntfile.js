761d05837fd067e78a4a098795a5f5580aa38c31
Peter Schuebl <peter_schuebl@dell.com>
2014-05-30 18:04:02 +0200

    Updated grunt build to allow faster development roundtrips

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,19 @@
 module.exports = function (grunt) {
 	
 	// load all grunt tasks
+	// Load grunt tasks automatically
+	require('load-grunt-tasks')(grunt);
+	/*
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-istanbul-coverage');
 	grunt.loadNpmTasks('grunt-karma');
 	grunt.loadNpmTasks('grunt-karma-coveralls');
+	*/
 	
 	// Default task.
-	grunt.registerTask('default', ['uglify', 'clean', 'test']);
+	grunt.registerTask('default', ['uglify', 'clean:coverage', 'test']);
 	grunt.registerTask('test', ['jshint', 'karma', 'coverage']);
 	grunt.registerTask('travis-test', ['jshint', 'karma', 'coverage', 'coveralls']);
 	
@@ -21,7 +25,25 @@ module.exports = function (grunt) {
 	
 	// Project configuration.
 	grunt.initConfig({
-		clean: ["coverage/*"],
+		clean: {
+			coverage: {
+				files: ["coverage/*"]
+			},
+			dist: {
+				files: [
+					{
+						dot: true,
+						src: [
+							'.tmp',
+							'<%= yeoman.dist %>/{,*/}*',
+							'!<%= yeoman.dist %>/.git*'
+						]
+					}
+				]
+			},
+			server: '.tmp'
+		},
+		//clean: ["coverage/*"],
 		coverage: {
 		  options: {
 		  	thresholds: {
@@ -74,6 +96,153 @@ module.exports = function (grunt) {
 					'dist/textAngular-sanitize.min.js': ['src/textAngular-sanitize.js']
 				}
 			}
+		},
+		// Watches files for changes and runs tasks based on the changed files
+		watch: {
+			bower: {
+				files: ['bower.json'],
+				tasks: ['bowerInstall']
+			},
+			js: {
+				files: ['src/{,*/}*.js'],
+				tasks: ['newer:jshint:all'],
+				options: {
+					livereload: '<%= connect.options.livereload %>'
+				}
+			},
+			jsTest: {
+				files: ['test/{,*/}*.js'],
+				tasks: ['newer:jshint:test', 'karma']
+			},
+			gruntfile: {
+				files: ['Gruntfile.js']
+			},
+			livereload: {
+				options: {
+					livereload: '<%= connect.options.livereload %>'
+				},
+				files: [
+					'src/{,*/}*.html',
+					'.tmp/styles/{,*/}*.css'
+				]
+			}
+		},
+		// Automatically inject Bower components into the app
+		bowerInstall: {
+			app: {
+				src: ['src/index.html'],
+				ignorePath: new RegExp('^src/')
+			}
+		},
+		connect: {
+			options: {
+				port: 9000,
+				hostname: '0.0.0.0',
+				livereload: 35729
+			},
+			livereload: {
+				options: {
+					open: 'http://localhost:9000',
+					base: [
+						'.tmp',
+						'src'
+					]
+				}
+			},
+			test: {
+				options: {
+					port: 9001,
+					base: [
+						'.tmp',
+						'test',
+						'src'
+					]
+				}
+			},
+			dist: {
+				options: {
+					base: 'dist'
+				}
+			}
+		},
+		useminPrepare: {
+			html: 'src/index.html',
+			options: {
+				dest: 'dist',
+				flow: {
+					html: {
+						steps: {
+							js: ['concat', 'uglifyjs'],
+							css: ['cssmin']
+						},
+						post: {
+						}
+					}
+				}
+			}
+		},
+		// Performs rewrites based on filerev and the useminPrepare configuration
+		usemin: {
+			html: ['dist/{,*/}*.html'],
+			css: ['dist/styles/{,*/}*.css'],
+			options: {
+				assetsDirs: ['dist', 'dist/images']
+			}
+		},
+		// ngmin tries to make the code safe for minification automatically by
+		// using the Angular long form for dependency injection. It doesn't work on
+		// things like resolve or inject so those have to be done manually.
+		ngmin: {
+			dist: {
+				files: [
+					{
+						expand: true,
+						cwd: '.tmp/concat/scripts',
+						src: '*.js',
+						dest: '.tmp/concat/scripts'
+					}
+				]
+			}
+		},
+		// Copies remaining files to places other tasks can use
+		copy: {
+			dist: {
+				files: [
+					{
+						expand: true,
+						dot: true,
+						cwd: 'src',
+						dest: 'dist',
+						src: [
+							'*.{ico,png,txt}',
+							'.htaccess',
+							'*.html',
+							'{,*/}*.html'
+						]
+					}
+				]
+			},
+			styles: {
+				expand: true,
+				cwd: 'src/styles',
+				dest: '.tmp/styles/',
+				src: '{,*/}*.css'
+			}
 		}
+
+	});
+
+
+	grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+		if (target === 'dist') {
+			return grunt.task.run(['build', 'connect:dist:keepalive']);
+		}
+
+		grunt.task.run([
+			'clean:server',
+			'bowerInstall',
+			'connect:livereload',
+			'watch'
+		]);
 	});
 };
